08bdff08d9f654cc353914bd0c3c42553d7236d1
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-05-25 16:08:17 +0200

    aggiunto commenti che spiegano cosa fanno le funzioni nei controller, e modificati gli status nella show

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,27 +1,29 @@
 import { posts } from "../data/postsData.js";
 
+
+/* 
+FUNZIONE CHE MOSTRA LA LISTA COMPLETA DEI POSTS
+COME PAGINA PRINCIPALE
+metodo : 'GET'   http://localhost:3000/posts
+*/
 function index(request, response) {
     response.json(posts)
 };
 
+/* 
+FUNZIONE CHE MOSTRA IL POST CHE ABBIAMO CERCATO 
+E PASSATO TRAMITE PARAMETRO NELL'URL
+metodo : 'GET'  http://localhost:3000/posts/1
+*/
 function show(request, response) {
     const { id } = request.params;
 
     const realId = Number(id.trim());
 
-    if (isNaN(realId)) {
-        response.status(404)
+    if (isNaN(realId) || realId <= 0) {
+        response.status(400)
             .json({
-                errore: '"id" non corretto',
-                risultato: null
-            });
-        return;
-    }
-
-    if (realId <= 0) {
-        response.status(404)
-            .json({
-                errore: '"id" non corretto è minore di 0',
+                errore: '"id" non corretto o minore di 0',
                 risultato: null
             });
         return;
@@ -44,6 +46,11 @@ function show(request, response) {
     });
 }
 
+/* 
+FUNZIONE CHE MOSTRA PERMETTE DI AGGIUNGERE UN POST NEL NOSTRO
+ARRAY DI POSTS
+metodo : 'POST'  http://localhost:3000/posts
+*/
 function store(request, response) {
     response.json({
         messaggio: 'hai inviato una richiesta di creazione'
@@ -62,6 +69,11 @@ function modify(request, response) {
     })
 }
 
+/* 
+FUNZIONE CHE MOSTRA PERMETTE DI ELIMINARE UN POST NEL NOSTRO
+ARRAY DI POSTS
+metodo : 'DELETE'  http://localhost:3000/posts
+*/
 function destroy(request, response) {
     const {id} = request.params;
     response.json({
